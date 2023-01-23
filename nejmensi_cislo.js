d7c533abb4f367aad5341266f975d38a906833c8
SkovajsaOndra <ondrej.skovajsa@educanet.cz>
2023-01-23 22:13:34 +0100

    Create nejmensi_cislo.js

---
+++ nejmensi_cislo.js
@@ -0,0 +1,5 @@
+cislo1 = prompt("Zadejte první číslo");
+cislo2 = prompt("Zadejte druhé číslo");
+cislo3 = prompt("Zadejte třetí číslo");
+let nejmensi = Math.min(cislo1,cislo2 ,cislo3 );
+document.write("Nejmenší číslo je "+nejmensi )
