07e97e8c564433365ed09b960a4dc38c7ead50e2
SkovajsaOndra <ondrej.skovajsa@educanet.cz>
2023-02-15 12:02:20 +0100

    Create nejvetsi_cislo.js

---
+++ nejvetsi_cislo.js
@@ -0,0 +1,5 @@
+cislo1 = prompt("Zadejte první číslo");
+cislo2 = prompt("Zadejte druhé číslo");
+cislo3 = prompt("Zadejte třetí číslo");
+let nejmensi = Math.max(cislo1,cislo2 ,cislo3 );
+document.write("Největší číslo je "+nejmensi )
